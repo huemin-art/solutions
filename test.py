6c530797f13694cdcbfed595775bdd965662c9db
huemin <huemin.art@gmail.com>
2025-10-01 16:38:22 -0700

    init

---
+++ test.py
@@ -0,0 +1,19 @@
+import torch
+# from diffusers import FluxKontextPipeline
+from pipeline import FluxKontextPipeline
+from diffusers.utils import load_image
+
+pipe = FluxKontextPipeline.from_pretrained("black-forest-labs/FLUX.1-Kontext-dev", torch_dtype=torch.bfloat16)
+pipe.to("cuda")
+
+input_image = load_image("input.png")
+
+image = pipe(
+  image=input_image,
+  width=1280, # same as input image
+  height=720, # same as input image
+  prompt="add a unicorn to the image",
+  guidance_scale=1.5
+).images[0]
+
+image.save("output_bad.png")
